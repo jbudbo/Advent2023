914d91e04099a4ae8eb82bef0967654365d5e726
Justin Morris <Justin.Morris@BD.com>
2023-12-15 16:03:00 -0500

    Day 15

---
+++ cs/Day15.cs
@@ -0,0 +1,130 @@
+﻿
+using System.Collections.Specialized;
+
+internal class Day15 : AdventBase<char[]>
+{
+    protected override string Day => nameof(Day15);
+
+    protected override Uof<char[]> ParseData(DataLine<string?> row, bool PartTwo)
+    {
+        return row.Data.Valid
+            ? Union.Pass(row.Data.Value!.ToCharArray())
+            : Union.Fail<char[]>();
+    }
+
+    protected override async ValueTask PartOne(IAsyncEnumerable<DataLine<char[]>> source, CancellationToken cancellationToken = default)
+    {
+        long value = 0;
+        await foreach (var (_,(valid, rowData)) in source)
+        {
+            foreach(char c in rowData)
+            {
+                if (c is ',')
+                {
+                    //reset
+                    Answer += value;
+                    value = 0;
+                    continue;
+                }
+
+                value += c;
+                value *= 17;
+                value %= 256;
+            }
+        }
+        Answer += value;
+    }
+
+    protected override async ValueTask PartTwo(IAsyncEnumerable<DataLine<char[]>> source, CancellationToken cancellationToken = default)
+    {
+        Dictionary<long, List<(string, int)>> boxes = [];
+        char[] buffer = new char[20];
+        int index = 0;
+        await foreach (var (_, (valid, rowData)) in source)
+        {
+            foreach (char c in rowData)
+            {
+                if (c is ',')
+                {
+                    //reset
+                    var (lens, additive, length) = Parse(buffer.AsSpan()[..index]);
+                    long box = 0;
+                    foreach (char l in lens)
+                    {
+                        box += l;
+                        box *= 17;
+                        box %= 256;
+                    }
+
+                    if (!boxes.ContainsKey(box))
+                        boxes[box] = [];
+
+                    if (!additive)
+                        boxes[box].RemoveAll(t => t.Item1 == lens);
+                    else
+                    {
+                        int idx = boxes[box].FindIndex(t => t.Item1 == lens);
+                        if (idx > -1)
+                        {
+                            boxes[box][idx] = (lens, length);
+                        }
+                        else
+                            boxes[box].Add((lens, length));
+                    }
+
+                    index = 0;
+                    continue;
+                }
+
+                buffer[index++] = c;
+            }
+        }
+        {
+            var (lens, additive, length) = Parse(buffer.AsSpan()[..index]);
+            long box = 0;
+            foreach (char l in lens)
+            {
+                box += l;
+                box *= 17;
+                box %= 256;
+            }
+
+            if (!boxes.ContainsKey(box))
+                boxes[box] = [];
+
+            if (!additive)
+                boxes[box].RemoveAll(t => t.Item1 == lens);
+            else
+            {
+                int idx = boxes[box].FindIndex(t => t.Item1 == lens);
+                if (idx > -1)
+                {
+                    boxes[box][idx] = (lens, length);
+                }
+                else
+                    boxes[box].Add((lens, length));
+            }
+        }
+
+        foreach(var (b, ll) in boxes)
+        {
+            for (int i = 1, j = ll.Count; i <= j; i++)
+            {
+                var (_, v) = ll[i-1];
+
+                long l = (b + 1) * i * v;
+                Answer += l;
+            }
+        }
+    }
+
+    private static readonly char[] delims = ['=', '-'];
+    private (string lens, bool remove, int val) Parse(ReadOnlySpan<char> chars)
+    {
+        Span<Range> buffer = stackalloc Range[2];
+        int bits = chars.SplitAny(buffer, delims, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+        Range b = buffer[0];
+        bool additive = bits > 1;
+        return (new (chars[b]), additive, additive ? int.Parse(chars[buffer[1]]) : -1);
+    }
+}
--- cs/Program.cs
+++ cs/Program.cs
@@ -1,6 +1,4 @@
-﻿using cs;
-
-CancellationTokenSource cts = new();
+﻿CancellationTokenSource cts = new();
 Console.CancelKeyPress += (_, e) =>
 {
     e.Cancel = true;
@@ -9,55 +7,7 @@ Console.CancelKeyPress += (_, e) =>
 
 long start = Stopwatch.GetTimestamp();
 
-var Answer = await new Day13().PartOne(cts.Token);
-
-//var Answer = await new Day12().PartOne(cts.Token);
-//var Answer = await new Day11().PartTwo(cts.Token);
-
-//var Answer = await new Day10().PartOne(cts.Token);
-
-//var Answer = await new Day9().PartTwo(cts.Token);
-
-//var Answer = await new Day8().PartTwo(cts.Token);
-
-//var Answer = await Day7.P2(ms, cts.Token);
-
-//var Answer = await Day6.P2(ms, cts.Token);
-
-//var Answer = await new Day5().PartTwo(cts.Token);
-
-
-//using var ms = new MemoryStream(Encoding.UTF8.GetBytes(Day5.P1_EXAMPLE));
-//using var ms = File.OpenRead("Day5.data");
-//long d5p1 = await Day5.P1(ms, cts.Token);
-//long d5p2 = await Day5.P2(ms, cts.Token);
-//Console.WriteLine($"Answer {d5p2}");
-
-//int d4p1 = Day4.P2(Day4.P1_EXAMPLE);
-//int d4p1 = Day4.P2(File.ReadAllText("Day4.data"));
-//Console.WriteLine($"Answer {d4p1}");
-
-//int d3p1 = Day3.P2(File.ReadAllText("Day3.data"));
-//Console.WriteLine($"Answer {d3p1}");
-
-//int d2p1 = Day2.Aggregate(Day2.P1_EXAMPLE);
-//Console.WriteLine($"Answer {d2p1}; Target {Day2.P1_ANSWER}");
-
-//int d2p2 = Day2.Max(File.ReadAllText("Day2.data"));
-//Console.WriteLine($"Answer {d2p2}");
-
-//int d1p1 = Day1.Calibrate(Day1.P1_EXAMPLE);
-//Debug.Assert(d1p1 == Day1.P1_ANSWER);
-
-//int d1p1 = Day1.Calibrate(File.ReadAllText("Day1.data"));
-//Debug.WriteLine(d1p1);
-
-//int d1p2 = Day1.ReCalibrate(Day1.P2_EXAMPLE);
-//Debug.Assert(d1p2 == Day1.P2_ANSWER);
-
-
-//int d1p2 = Day1.ReCalibrate(File.ReadAllText("Day1.data"));
-//Debug.WriteLine(d1p2);
+var Answer = await new Day15().PartTwo(cts.Token);
 
 Console.WriteLine($"Answer {Answer}");
 Console.WriteLine($"Run time {Stopwatch.GetElapsedTime(start)}");
